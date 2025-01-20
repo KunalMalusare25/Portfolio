2d463060387914987c7016d3c8dba32a070ca127
KunalMalusare25 <kunalmalusare2505@gmail.com>
2025-01-20 13:04:18 +0530

    Kunal Malusare

---
+++ src/app/components/Data.js
@@ -0,0 +1,70 @@
+export const projectdata = [
+  {
+    id: 1,
+    title: "Online Placement Management System with AWS Connectivity",
+    description:
+      "• Developed a web application using Django, HTML, and CSS with separate portals for students and TPOs. • Student portal features include viewing new companies, managing profiles, and accessing help. • TPO portal features include registering companies, scheduling visits, and accessing applicant lists. • Connected to a MySQL database hosted on AWS RDS for scalable data management. • Implemented secure and efficient file storage with AWS S3. • Ensured high availability and scalability by deploying on AWS EC2.",
+    image: "/images/pms.png",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 2,
+    title: "Karwai 101",
+    description:
+      "• Contributing to a live project for the Maharashtra Federation, built and maintained responsive dashboards and components. • Developed a multi-user login system with separate dashboards for different user types (federation and society logins). • Built dynamic, responsive UIs in ReactJS using state management, React Hooks, Context API, and React Router for efficient app functionality . • Integrated RESTful APIs using Axios and Fetch API for seamless data fetching.",
+    image: "/images/101.png",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 3,
+    title: "NetFlix Clone",
+    description:
+      "• Designed a sleek and modern UI inspired by Netflix, incorporating eye-catching visuals and intuitive navigation for an immersive user experience. • Ensured seamless functionality across devices with a responsive layout, adapting flawlessly to desktops, tablets, and mobile screens.• Integrated JavaScript functionalities like dynamic content display, hover effects, and interactive carousels for enhanced user engagement. • Built using only HTML, CSS, and JavaScript, showcasing core web development skills without reliance on external frameworks.",
+    image: "/images/netflix.jpeg",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 4,
+    title: "Food Recipe App",
+    description:
+      "• Created a mobile app with responsive, user-friendly screens for iOS and Android, showcasing recipes and video tutorials. • API Integration with Axios: Leveraged Axios to fetch recipe data, including images, ingredients, and YouTube links, from a local API, ensuring dynamic and updated content. • Implemented multi-screen navigation using @react-navigation/native-stack for smooth user flow across recipe categories and details. • Designed an interactive interface with Tailwind CSS and animations like bounce and fade-in/out, enhancing app aesthetics and user experience. • Integrated embedded YouTube videos, calorie information, and a Wishlist feature, providing comprehensive and engaging recipe content. ",
+    image: "/images/foodapp1.jpeg",
+    tag: ["All", "App"],
+  },
+  {
+    id: 5,
+    title: "Bookly- AI Book Recommendation",
+
+    description:
+      "• Integrated an AI algorithm that suggests books based on user preferences, including reading goals and favorite genres like horror, action, and more. • Enabled users to follow others, fostering a social aspect within the app and allowing them to share and discover books.• Implemented an intuitive onboarding experience, where new users receive helpful tips on how to navigate and use the app effectively. • Developed a secure user registration system, which includes email verification and a forgot password feature to enhance user security.",
+    image: "/images/booklyapp1.jpeg",
+    tag: ["All", "App"],
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    title: "HTML & CSS",
+  },
+  {
+    id: 2,
+    title: "JavaScript",
+  },
+  {
+    id: 3,
+    title: "React",
+  },
+  {
+    id: 4,
+    title: "React-native",
+  },
+  {
+    id: 5,
+    title: "Node.js & Express",
+  },
+  {
+    id: 6,
+    title: "Git & Version Control",
+  },
+];
